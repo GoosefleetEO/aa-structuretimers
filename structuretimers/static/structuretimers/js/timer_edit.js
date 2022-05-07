a237c44bf3b76c413a2fd6a75128239371949ed1
ErikKalkoken <erik.kalkoken@gmail.com>
2022-05-07 21:04:38 +0200

    Add locale support for datetimepicker

--- structuretimers/static/structuretimers/js/timer_edit.js
+++ structuretimers/static/structuretimers/js/timer_edit.js
@@ -3,6 +3,14 @@ $(document).ready(function () {
     const select2SolarSystemsUrl = elem.getAttribute('data-select2SolarSystemsUrl');
     const select2StructureTypesUrl = elem.getAttribute('data-select2StructureTypesUrl');
     const myTheme = "bootstrap"
+    const isNightMode = JSON.parse(document.getElementById('night-mode-data').textContent);
+    const datetimepickerTheme = isNightMode ? 'dark' : 'default'
+    let languageCode = JSON.parse(document.getElementById('language-code-data').textContent);
+
+    // mapping of language codes from Django to datetimepicker widget
+    if (languageCode === "zh-hans") {
+        languageCode = "zh";
+    }
 
     $('.select2-solar-systems').select2({
         ajax: {
@@ -31,8 +39,7 @@ $(document).ready(function () {
         dropdownCssClass: "my_select2_dropdown"
     });
 
-    const value = JSON.parse(document.getElementById('night-mode-data').textContent);
-    const datetimepickerTheme = value ? 'dark' : 'default'
+    $.datetimepicker.setLocale(languageCode);
     $('#timer-date-field').datetimepicker({ format: 'Y-m-d H:i', theme: datetimepickerTheme });
 
     // Clear date field when time-remaining fields are used and vice versa
